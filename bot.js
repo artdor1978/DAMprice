64d88714d75d5ae363486de0e2ef3885a4654749
ArtDor1978 <artdor1978@gmail.com>
2022-01-13 10:50:52 +0200

    "init"

--- bot.js
+++ bot.js
@@ -43,3 +43,23 @@ bot.launch();
 
 
 //https://devcenter.heroku.com/articles/deploying-nodejs
+//https://javascript.plainenglish.io/how-to-use-node-js-with-google-sheets-c256c26e10fc
+//https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/update
+
+//https://daily-dev-tips.com/posts/nodejs-write-data-in-a-google-sheet/
+//https://hackernoon.com/how-to-use-google-sheets-api-with-nodejs-cz3v316f
+
+
+//https://olefyrenko.com/blog/how-to-create-a-telegram-crypto-bot-in-javascript
+//https://www.sohamkamani.com/blog/2016/09/21/making-a-telegram-bot/
+//https://www.section.io/engineering-education/telegram-bot-in-nodejs/
+
+//https://medium.com/geekculture/build-a-telegram-bot-using-typescript-node-js-and-telegraf-and-deploy-it-on-heroku-fcc28c15614f
+//https://dev.to/benjami51033333/build-a-bot-with-telegram-and-ibm-watson-io5
+//https://flaviocopes.com/google-api-authentication/
+
+//https://googleapis.dev/nodejs/googleapis/latest/tasks/
+
+//https://polyakovdmitriy.ru/bot-telegram/
+//https://archakov.im/post/kak-razmestit-telegram-bota-na-servere
+//https://archakov.im/post/telegram-bot-on-nodejs
