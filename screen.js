80d6ac9e059a73e10def459f21606a3d4b89a50a
ArtDor1978 <artdor1978@gmail.com>
2022-01-31 09:36:40 +0200

    change month2

--- screen.js
+++ screen.js
@@ -16,7 +16,7 @@ module.exports = async function scrape() {
         width: 1920,
         height: 1480
     });
-    await page.goto('https://docs.google.com/spreadsheets/d/1PXj2pH6VJd5O8QyWn-4qElAvHwJQiBRsZIHU93rAlC8/edit#gid=1823202329');
+    await page.goto('https://docs.google.com/spreadsheets/d/1PXj2pH6VJd5O8QyWn-4qElAvHwJQiBRsZIHU93rAlC8/edit#gid=650837413');
     await page.waitForTimeout(1000);
     await page.screenshot({ path: 'example.png', fullPage: true });
     await browser.close();
